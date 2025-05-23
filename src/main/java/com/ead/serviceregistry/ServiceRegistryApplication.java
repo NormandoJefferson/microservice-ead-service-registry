a02a53de30f000270612af3386682d232b46e2ec
NormandoJefferson <normandojefferson@hotmail.com>
2025-05-23 08:33:33 -0300

    chore: add comments

--- src/main/java/com/ead/serviceregistry/ServiceRegistryApplication.java
+++ src/main/java/com/ead/serviceregistry/ServiceRegistryApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer // Habilita um servidor eureka.
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
